7b7ce73b4ac5f91c7a760f8c487143c40c1b99a7
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2022-02-12 18:52:51 +0100

    Override default layout with global styles

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <Navbar />
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+* , *::after, *::before {
+  box-sizing: border-box;
+}
+
+body {
+  background: black;
+  margin-left: 80px;
+  margin-right: 80px;
+}
+
+@media screen and (max-width: 769px) {
+  body {
+    padding-top: 80px;
+    margin-left: 16px;
+    margin-right: 16px;
+  }
+}
+
+.link {
+  text-decoration: none;
+  color: #0CC6FF;
+  position: relative;
+}
+
+.link:after {
+  position: absolute;
+  display: block;
+  left: 0;
+  bottom: 0;
+  width: 0;
+  height: 1px;
+  content: '';
+  background:#0CC6FF;
+
+  transition: all 0.5s;
+}
+
+.link:hover:after {
+  width: 100%;
+}
+
+ul {
+  padding-inline-start: 0;
+}
+
+</style>
