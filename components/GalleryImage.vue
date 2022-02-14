98e8cc0d1f2ef6232e1360133348f0efb4568949
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2022-02-14 21:03:15 +0100

    Add Gallery page with reusable GalleryImage component

---
+++ components/GalleryImage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="gallery-image">
+    <img :src="src"/>
+    <div class="gallery-image__text">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'GalleryImage',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: false,
+      default: 'Sample Text'
+    }
+  }
+})
+</script>
+
+<style src="./GalleryImage.css" scoped/>
--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,3 +1,43 @@
 <template>
-  <div></div>
+  <div class="gallery">
+    <header class="gallery__info">
+      <h1>Galeria. Tu zobaczysz jak działamy</h1>
+    </header>
+    <div class="gallery__grid">
+      <GalleryImage v-for="(image, index) in images" :key="index" :text="image.text" :src="image.src"/>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import GalleryImage from '~/components/GalleryImage.vue';
+export default Vue.extend({
+    name: "Gallery",
+    data() {
+      return {
+        images: [
+          {
+            text: "Zajęcia z bezpieczeństwa API z Maciejem Koflem",
+            src: "https://knping.pl/images/kofel_prezentacja4.png"
+          },
+          {
+            text: "Selfie ze spotkania z Maciejem Koflem",
+            src: "https://knping.pl/images/kofelselfie3.jpg",
+          },
+          {
+            text: "Ping oraz Maciej Kofel ze szkolasecurity.pl",
+            src: "https://knping.pl/images/ping_i_kofel2.png"
+          },
+          {
+            text: "Spotkanie z Maciejem Koflem ze szkolasecurity.pl",
+            src: "https://knping.pl/images/IMG_2035.png"
+          }
+        ]
+      };
+  },
+  components: { GalleryImage }
+})
+</script>
+
+<style src="./gallery.css" scoped/>
