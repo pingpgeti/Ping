a2c2ef86b8484020c6fc5400993650488bb52354
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2022-03-05 20:55:51 +0100

    Add News list in Index page

---
+++ components/News.vue
@@ -0,0 +1,38 @@
+<template>
+  <article class="news">
+    <header class="news__header">
+      {{ title }}
+    </header>
+    <span class="news__date"> {{ date}} </span>
+    <div class="news__content">
+      
+      <p class="news__text">  
+        <img class="news__img" :src="src"/>
+        {{ text }}
+      </p>
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'News',
+  props: {
+    title: {
+      type: String
+    },
+    date: {
+      type: String
+    },
+    src: {
+      type: String
+    },
+    text: {
+      type: String
+    }
+  }
+})
+</script>
+
+<style src="./News.css" scoped />
