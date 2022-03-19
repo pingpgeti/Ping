c9aa4dccb224611d839334ffd6dc41ecb0afec34
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2022-03-19 21:19:29 +0100

    Add crossfetch option to apollo

--- apollo/client.ts
+++ apollo/client.ts
@@ -3,11 +3,13 @@ import {
   InMemoryCache,
 } from 'apollo-cache-inmemory';
 import ApolloClient from 'apollo-client';
+import fetch from 'cross-fetch';
 
 const client = new ApolloClient({
   link: PrismicLink({
     uri: 'https://suskatest.prismic.io/graphql',
-  }),
+    fetch: fetch
+  }) as any,
   cache: new InMemoryCache(),
 });
 
