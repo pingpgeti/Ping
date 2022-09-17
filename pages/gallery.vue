566edfcf8f051530b6cfef9828053e1aaea8ceb7
Jakub Kuniszewski <you@example.com>
2022-09-17 12:45:56 +0200

    Move gallery contents and translations to prismic

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="gallery">
     <header class="gallery__info">
-      <h1>Galeria - tu zobaczysz jak działamy</h1>
+      <h1>{{ translatedTitle }}</h1>
     </header>
     <client-only>
         <LightGallery
@@ -11,10 +11,10 @@
           @close="closeGallery"/>
     </client-only>
     <div class="gallery__grid">
-      <GalleryImage v-for="(image, gIndex) in images"
+      <GalleryImage v-for="(image, gIndex) in translatedImages"
         @click.native="galleryClick(gIndex)"
         :key="gIndex"
-        :text="image.text"
+        :text="image.label"
         :src="image.src"/>
     </div>
     <Footer/>
@@ -26,36 +26,60 @@ import Vue from 'vue'
 import GalleryImage from '~/components/GalleryImage.vue';
 
 import GetGalleries from '~/graphql/getGalleries.gql';
+import getGalleryPage from '~/graphql/getGalleryPage.gql';
+
 import { client } from '~/apollo/client';
-import { mapMutations } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 
 export default Vue.extend({
     name: "Gallery",
     components: { GalleryImage },
     data: () => ({
+      title: {},
       images: [],
       galleryIndex: null
     }),
     async asyncData() {
+      const { data: titleData } =  await client.query({query: getGalleryPage});
+      const title = {
+        pl: titleData.allGallerypages.edges[0].node.pl[0].text,
+        en: titleData.allGallerypages.edges[0].node.en[0].text,
+      }
+
       const { data } = await client.query({query: GetGalleries});
       const images = data.allGallerys.edges.map(edge => {
         const children = edge.node.images.map(img => img.image.url);
         return {
-          text: edge.node.title[0].text,
+          pl: edge.node.pl[0].text,
+          en: edge.node.en[0].text,
           src: edge.node.images[0].image.url,
           children
         }
       });
 
       return {
+        title,
         images
       }
     },
     computed: {
+      ...mapGetters([
+        'lang'
+      ]),
       gallery() {
         if(this.images[this.galleryIndex])
           return this.images[this.galleryIndex].children;
         return [];
+      },
+      translatedImages() {
+        return this.images.map(x => ({
+          src: x.src,
+          children : x.children,
+          label: x[this.lang.substring(0,2)]
+        }));
+      },
+      translatedTitle() {
+        return this.title[this.lang.substring(0,2)]
       }
     },
     methods: {
